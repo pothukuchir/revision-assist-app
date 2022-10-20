db6383e94deedb09994a11068cf6d65fbd309561
Ravikant Pothukuchi <pothukuchirsnh@gmail.com>
2022-10-20 06:34:04 +0200

    Adding React Code

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReadTextUsingTesseract from "./Components/ReadTextUsingTesseract/ReadTextUsingTesseract";
+
+function App() {
+  return (
+    <div className="App">
+      <ReadTextUsingTesseract className="ToBeCentered" />
+    </div>
+  );
+}
+
+export default App;
